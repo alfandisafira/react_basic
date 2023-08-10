6a94a2e91b95b29bade875a0fbeabcf993c7ee84
Alfandi Safira <fandi.nutech@gmail.com>
2023-08-10 14:40:56 +0700

    Rendering list

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,33 +1,45 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    title: "Macbook Pro m1",
+    price: "Rp. 15.000.000",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. In temporibus voluptatum quas iure minus, vel recusandae autem repudiandae magni atque facilis inventore, nesciunt corrupti officia? Mollitia laborum alias totam ipsam?",
+    imageSource: "/assets/images/91374.jpg",
+  },
+  {
+    id: 2,
+    title: "Asus Vivobook",
+    price: "Rp. 7.000.000",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque nemo sapiente est. Corrupti, aliquam sit!",
+    imageSource: "/assets/images/91374.jpg",
+  },
+  {
+    id: 3,
+    title: "Lenovo Legion",
+    price: "Rp. 12.000.000",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. In temporibus voluptatum quas iure minus, vel recusandae autem repudiandae magni atque facilis inventore, nesciunt corrupti officia? Mollitia laborum alias totam ipsam?",
+    imageSource: "/assets/images/91374.jpg",
+  },
+];
+
 const ProductPage = () => {
   return (
     <div className="min-h-screen bg-cyan-500">
-      <div className="flex justify-center">
-        {/* <div className="w-full max-w-xs bg-zinc-200 bg-opacity-30 p-6 rounded-xl mt-4">
-          <a href="#">
-            <img src="/assets/images/91374.jpg" alt="" />
-          </a>
-          <div className="mt-4">
-            <h1 className="font-semibold">Macbook Pro M1 2020</h1>
-            <p className="text-justify font-normal">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Voluptates, voluptatum. Officia architecto magnam ratione ipsum.
-            </p>
-          </div>
-          <div className="mt-2 flex justify-between items-center">
-            <h2 className="font-semibold pt-2">Rp. 15.000.000</h2>
-            <Button bgColor="bg-slate-900 hover:bg-slate-500">Buy now</Button>
-          </div>
-        </div> */}
-        <CardProduct>
-          <CardProduct.Header imageSource="/assets/images/91374.jpg" />
-          <CardProduct.Body title="Macbook Pro M1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-            quaerat optio error laboriosam consequuntur voluptates.
-          </CardProduct.Body>
-          <CardProduct.Footer price="Rp. 15.000.000" />
-        </CardProduct>
+      <div className="flex justify-center gap-4">
+        {products.map((product) => (
+          <CardProduct key={product.id}>
+            <CardProduct.Header imageSource={product.imageSource} />
+            <CardProduct.Body title={product.title}>
+              {product.description}
+            </CardProduct.Body>
+            <CardProduct.Footer price={product.price} />
+          </CardProduct>
+        ))}
       </div>
     </div>
   );
